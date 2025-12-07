a7ae4291c3bbc1f96b20768db908c0eeaa481354
abunooh <nwkhmhmd@gmail.com>
2025-12-08 01:22:00 +0200

    forgot to connect in app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import authRoutes from './routes/auth.routes.js';
+import caseRoutes from "./routes/cases.routes.js";
 const app = express();
 app.use(express.json());
 app.use('/auth', authRoutes);
+app.use("/cases", caseRoutes);
 export default app;
